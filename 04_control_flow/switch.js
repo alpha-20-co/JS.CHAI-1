aa0ad878ebed129fd285c8effc6d1d57542ca90c
shruti kumari <coalpha09@gmail.com>
2026-01-25 11:27:05 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+// agar break nhii likha rhega to uske baad ka saara case execute ho jaega except default....isliye break lagana jruri h.
